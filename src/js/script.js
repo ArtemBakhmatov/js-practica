3f0aabcd72d0cf011b447a6f9a758c9df58ed5e7
Artem <ip.pakhmatov@mail.ru>
2022-10-01 19:26:51 +0300

    Проект FOOD - Отправка данных на сервер через Fetch

--- src/js/script.js
+++ src/js/script.js
@@ -244,10 +244,6 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -255,21 +251,24 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
             })
-        })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+        });
     }
 
     function showThanksModal(message) {
@@ -295,6 +294,15 @@ window.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.remove('hide');
             closeModal();
         }, 4000);
-
     }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',                         // метод
+        body: JSON.stringify({name: 'Alex'}),   // что отправляем
+        Headers: {                              // заголовок
+            'Content-type': 'application/json'
+        }
+    })
+    .then(response => response.json()) // response -> ответ в json // response.json()) -> переводит в объект
+    .then(json => console.log(json));  // объект выводится в консоли
 });
